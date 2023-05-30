ef33f88ef5418e3b7551f2f32bf3cbe3a04d9237
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-05-30 19:17:08 +0900

    不要なものを削除して、エンコードのための関数だけ作った

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,13 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn sjis_to_utf8(src: &str) -> String {
+    format!("{}", src)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![sjis_to_utf8])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
