36b7cb46b35c1610af59549923a3ac2e5e46387d
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-05-30 20:11:48 +0900

    バイナリを渡そうとしているがうまくいかない

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use encoding_rs;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn sjis_to_utf8(src: &str) -> String {
-    format!("{}", src)
+fn sjis_to_utf8(src: &[u8]) -> String {
+    format!("aaa")
+    // format!("{}", src[0])
+    // let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&src);
+    // res.into_owned();
 }
 
 fn main() {
