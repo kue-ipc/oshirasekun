7041dd6b189cace56495243d2134a49a0b28ffe1
raccy <raccy@users.noreply.github.com>
2023-05-30 22:45:44 +0900

    配列渡してエンコードできた！

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,11 +5,9 @@ use encoding_rs;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn sjis_to_utf8(src: &[u8]) -> String {
-    format!("aaa")
-    // format!("{}", src[0])
-    // let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&src);
-    // res.into_owned();
+fn sjis_to_utf8(src: Vec<u8>) -> String {
+    let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&src);
+    res.into_owned()
 }
 
 fn main() {
