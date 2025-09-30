e40d647f5e760800c7c3c53dc35a16498f8695b4
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-09-30 13:58:29 +0900

    pnpm tauri migrate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,7 @@ fn sjis_to_utf8(src: Vec<u8>) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![sjis_to_utf8])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
